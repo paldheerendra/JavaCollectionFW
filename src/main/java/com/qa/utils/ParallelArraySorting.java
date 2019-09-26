bb85e45b2ee4998419a83ab48c189c707d0018ce
dheerendras06@hotmail.com <Dheerendra.Pal@sqs.com>
2019-09-26 17:47:22 +0530

    parallel array sorting

---
+++ src/main/java/com/qa/utils/ParallelArraySorting.java
@@ -0,0 +1,35 @@
+/**
+ * 
+ */
+package com.qa.utils;
+
+import java.util.Arrays;
+
+/**
+ * @author Dheerendra Pal (expleo)
+ *
+ */
+public class ParallelArraySorting {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+
+		 // Creating an integer array   
+        int[] arr = {7,8,1,0,6,3};  
+        // Iterating array elements  
+        for (int i : arr) {  
+            System.out.print(i+" ");  
+        }  
+        // Sorting array elements parallel  
+        Arrays.parallelSort(arr); 
+        System.out.println("\nArray elements after sorting");  
+        // Iterating array elements  
+        for (int i : arr) {  
+            System.out.print(i+" ");  
+        }  
+
+	}
+
+}
