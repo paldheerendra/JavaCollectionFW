469a9bac5c49b3eac6657f8533c350297cb3d3ed
Dheerendra Singh <dheerendras06@hotmail.com>
2020-06-04 12:07:13 +0530

    Added number to words programm

---
+++ src/main/java/com/programming/integers/IntToWords.java
@@ -0,0 +1,30 @@
+
+public class IntToWords {
+	
+
+	public static void main(String[] args) {
+
+		String str1 ="dheerendra";
+		String str2= new String("dheerendra");
+		
+		System.out.println(str1==str2);
+		
+		String str= "Dhe56eRe$&$#N";
+		System.out.println(Integer.toHexString(str.hashCode()));
+		str = str + "465465";
+		System.out.println(Integer.toHexString(str.hashCode()));
+		//str = str.replaceAll("[\\d]", "");
+		str = str.replaceAll("[^a-zA-z]", "");
+		
+		for(int i = str.length()-1 ; i>=0 ; i--) {
+			
+			 if(Character.isUpperCase(str.charAt(i))) {
+				 System.out.print(Character.toLowerCase(str.charAt(i)));
+			 }
+			 else
+				 System.out.print(Character.toUpperCase(str.charAt(i)));
+		}
+		
+	}
+
+}
